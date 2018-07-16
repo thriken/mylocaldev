22cbeaacb2b8a5356dd808adc01c97de4a9643d1
Administrator <Administrator@CS-02>
2018-07-17 03:49:17 +0800

    删除指定字符集UTF8MB4

--- mywow/mywow.sql
+++ mywow/mywow.sql
@@ -1,15 +1,3 @@
--- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.5.41-log - MySQL Community Server (GPL)
--- 服务器操作系统:                      Win32
--- HeidiSQL 版本:                  9.1.0.4867
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 -- 导出 mywow 的数据库结构
 CREATE DATABASE IF NOT EXISTS `mywow` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `mywow`;
